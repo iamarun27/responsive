cb561be09e8a4c18e8056f2c4809b1a5aec972ab
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-06-06 22:55:19 +0530

    matrix-text

--- Matrix-text/script.js
+++ Matrix-text/script.js
@@ -2,17 +2,22 @@ const para = document.querySelector("p");
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const text = para.innerText;
 
+var iteration = 0;
 para.addEventListener("mouseenter", () => {
   // console.log("Entered...")
   setInterval(() => {
     const str = text
       .split("")
       .map((char, index) => {
+        if (index < iteration) {
+          return char;
+        }
         return characters.split("")[Math.floor(Math.random() * 53)];
       })
       .join("");
 
     para.innerText = str;
+    iteration += 0.2;
     // console.log(str);
   }, 30);
 });
